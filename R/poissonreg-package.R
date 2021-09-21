3cecdd1a43fc15ab91397683d62cac90f9f601c4
Max Kuhn <mxkuhn@gmail.com>
2021-09-21 14:49:03 -0400

    more documentation

---
+++ R/poissonreg-package.R
@@ -0,0 +1,15 @@
+#' parsnip methods for Poisson regression
+#'
+#' \pkg{poissonreg} offers a function to fit model to count data using Poisson
+#' generalized linear models or via different methods for zero-inflated Poisson
+#' (ZIP) models.
+#'
+#' The model function works with the tidymodels infrastructure so that the model
+#' can be resampled, tuned, tided, etc.
+#'
+#' @includeRmd man/rmd/example.Rmd details
+#' @keywords internal
+"_PACKAGE"
+
+
+NULL
