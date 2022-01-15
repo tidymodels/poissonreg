69cf9c1f72e08193665b2f4cedc2433ad06c2a40
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-01-14 17:02:00 -0800

    remove engine documentation files

--- R/poisson_reg_glm.R
+++
@@ -1,9 +0,0 @@
-#' Poisson regression via glm
-#'
-#' [stats::glm()] uses maximum likelihood to fit a model for count data.
-#'
-#' @includeRmd man/rmd/poisson_reg_glm.Rmd details
-#'
-#' @name details_poisson_reg_glm
-#' @keywords internal
-NULL
