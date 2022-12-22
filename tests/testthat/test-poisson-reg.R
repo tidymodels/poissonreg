8e4a3067be32e482b64b6849c77ab90dae9e0c3c
Hannah Frick <hannah@rstudio.com>
2022-12-22 15:39:43 +0000

    move tests that don't require an engine to parsnip

--- tests/testthat/test-poisson-reg.R
+++ tests/testthat/test-poisson-reg.R
@@ -1,16 +1,3 @@
-test_that('updating', {
-  expect_snapshot(
-    poisson_reg(penalty = 1) %>%
-      set_engine("glmnet", lambda.min.ratio = 0.001) %>%
-      update(mixture = tune())
-  )
-})
-
-test_that('bad input', {
-  expect_error(poisson_reg(mode = "bogus"))
-  expect_error(translate(poisson_reg(mode = "regression"), engine = NULL))
-  expect_error(translate(poisson_reg(formula = y ~ x)))
-})
 
 # ------------------------------------------------------------------------------
 # glm execution tests
