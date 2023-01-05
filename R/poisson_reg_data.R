bd294bf63d4c7fd08ed3eaac8027d704bccb9bf6
Hannah Frick <hannah@rstudio.com>
2023-01-05 13:52:43 +0000

    fix nocov command

--- R/poisson_reg_data.R
+++ R/poisson_reg_data.R
@@ -3,7 +3,7 @@
 # they are already in the parsnip model database. We'll exclude them from
 # coverage stats for this reason.
 
-# nocov
+# nocov start
 make_poisson_reg_glm <- function() {
 
   parsnip::set_model_engine("poisson_reg", "regression", "glm")
