d8b8ec63c7c68783c9e5c2e15d970fa402f8bd43
Hannah Frick <hannah@rstudio.com>
2023-02-27 14:37:17 +0000

    parsnip will require a penalty arg
    tidymodels/parsnip#860

--- tests/testthat/test-poisson_reg-glmnet.R
+++ tests/testthat/test-poisson_reg-glmnet.R
@@ -1,7 +1,7 @@
 test_that("prediction of single row", {
   skip_if_not_installed("glmnet")
 
-  m <- poisson_reg() %>%
+  m <- poisson_reg(penalty = 0.1) %>%
     set_engine("glmnet") %>%
     fit(count ~ (.)^2, data = seniors[,2:4])
 
