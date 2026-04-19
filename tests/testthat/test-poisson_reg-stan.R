6c2658786c66af10400b26648b0ba3ba2b08f2c2
Hannah Frick <hannah@posit.co>
2026-04-19 16:49:29 +0100

    tidy up stan tests

--- tests/testthat/test-poisson_reg-stan.R
+++ tests/testthat/test-poisson_reg-stan.R
@@ -1,67 +1,35 @@
-## -----------------------------------------------------------------------------
-
-library(poissonreg)
-library(tidyr)
-
-# ------------------------------------------------------------------------------
-
-ctrl <- control_parsnip(verbosity = 1, catch = FALSE)
-caught_ctrl <- control_parsnip(verbosity = 1, catch = TRUE)
-quiet_ctrl <- control_parsnip(verbosity = 0, catch = TRUE)
-
-run_glmnet <- utils::compareVersion('3.6.0', as.character(getRversion())) < 0
-
-senior_ind <- model.matrix(~., data = seniors)[, -1]
-senior_ind <- tibble::as_tibble(senior_ind)
-
-glm_spec <- poisson_reg() %>% set_engine("glm")
-glmn_spec <- poisson_reg(penalty = 0.01, mixture = 0.3) %>% set_engine("glmnet")
-stan_spec <- poisson_reg() %>% set_engine("stan", refresh = 0)
-hurdle_spec <- poisson_reg() %>% set_engine("hurdle")
-zeroinfl_spec <- poisson_reg() %>% set_engine("zeroinfl")
-
-# ------------------------------------------------------------------------------
-
-test_that('stan_glm execution', {
+test_that("stan execution", {
   skip_if_not_installed("rstanarm")
   skip_on_cran()
 
-  expect_error(
-    res <- fit(
-      stan_spec,
-      count ~ .,
-      data = seniors,
-      control = ctrl
-    ),
-    regexp = NA
+  ctrl <- control_parsnip(verbosity = 1, catch = FALSE)
+  stan_spec <- poisson_reg() |> set_engine("stan", refresh = 0)
+
+  expect_no_error(
+    fit(stan_spec, count ~ ., data = seniors, control = ctrl)
   )
-  expect_error(
+  expect_no_error(
     res <- fit_xy(
       stan_spec,
       x = seniors[, 1:3],
       y = seniors$count,
       control = ctrl
-    ),
-    regexp = NA
+    )
   )
 
   expect_false(has_multi_predict(res))
   expect_identical(multi_predict_args(res), NA_character_)
 
-  expect_error(
-    res <- fit(
-      stan_spec,
-      Species ~ term,
-      data = seniors,
-      control = ctrl
-    )
-  )
+  expect_snapshot(error = TRUE, {
+    fit(stan_spec, Species ~ term, data = seniors, control = ctrl)
+  })
 })
 
-
-test_that('stan prediction', {
+test_that("stan prediction", {
   skip_if_not_installed("rstanarm")
   skip_on_cran()
+
+  quiet_ctrl <- control_parsnip(verbosity = 0, catch = TRUE)
 
   stan_pred <- c(
     6.291754969016,
@@ -72,38 +40,39 @@ test_that('stan prediction', {
   )
 
   res_xy <- fit_xy(
-    poisson_reg() %>% set_engine("stan", seed = 10, chains = 1, refresh = 0),
+    poisson_reg() |> set_engine("stan", seed = 10, chains = 1, refresh = 0),
     x = seniors[, 1:3],
     y = seniors$count,
     control = quiet_ctrl
   )
 
   expect_identical(
-    stan_pred,
     predict(res_xy, seniors[1:5, 1:3])$.pred,
+    stan_pred,
     tolerance = 0.001
   )
 
   res_form <- fit(
-    stan_spec,
+    poisson_reg() |> set_engine("stan", refresh = 0),
     count ~ .,
     data = seniors,
     control = quiet_ctrl
   )
   expect_identical(
-    stan_pred,
     predict(res_form, seniors[1:5, ])$.pred,
+    stan_pred,
     tolerance = 0.001
   )
 })
 
-
-test_that('stan intervals', {
+test_that("stan intervals", {
   skip_if_not_installed("rstanarm")
   skip_on_cran()
 
+  quiet_ctrl <- control_parsnip(verbosity = 0, catch = TRUE)
+
   res_xy <- fit_xy(
-    poisson_reg() %>%
+    poisson_reg() |>
       set_engine("stan", seed = 1333, chains = 10, iter = 1000, refresh = 0),
     x = seniors[, 1:3],
     y = seniors$count,
@@ -164,6 +133,8 @@ test_that('stan intervals', {
 test_that("stan intervals with standard error", {
   skip_if_not_installed("rstanarm")
   skip_on_cran()
+
+  quiet_ctrl <- control_parsnip(verbosity = 0, catch = TRUE)
 
   res_xy <- fit_xy(
     poisson_reg() |>
